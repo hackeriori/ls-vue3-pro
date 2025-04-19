0f0a237dfb7f374ed00d03f7e0aec485bcce8371
Administrator <453843655@qq.com>
2025-04-19 19:06:23 +0800

    feat(VirtualList): 优化虚拟列表组件-更新页面

--- docs/assets/FocusTest-BO0fVMal.js
+++
@@ -1,8 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./VuePlayground-s1O2g1sk.js","./base-Cu9M98Mf.js","./index-B-ef8-WX.js","./index-DH63uhMi.css","./base-CMRK1q-d.css","./el-scrollbar-CU9sRXL0.js","./install-BqcjuNST.js","./el-scrollbar-BWxh-h6K.css","./lsBaseLib.es-DNO7EYIp.js","./VuePlayground-D8Czq4-w.css"])))=>i.map(i=>d[i]);
-import{d as p,j as m,a as o,A as s,z as u,f as c,u as r,a5 as d,F as f,a6 as v,o as i}from"./index-B-ef8-WX.js";import"./base-Cu9M98Mf.js";import{v as _,i as g}from"./importMap-C6pPPIbD.js";const x=`<script setup>
-import {vFocus} from 'ls-vue3-pro'
-<\/script>
-
-<template>
-  <input v-focus type="text" placeholder="自动聚焦的输入框"/>
-</template>`,A=p({__name:"FocusTest",setup(V){const t=m(!0),n=d(async()=>{const a=await v(()=>import("./VuePlayground-s1O2g1sk.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return t.value=!1,a.default});return(a,e)=>{const l=_;return i(),o(f,null,[e[0]||(e[0]=s("div",{class:"ls-marginV-1"},"v-focus指令，用于元素自动聚焦的自定义指令。",-1)),e[1]||(e[1]=s("div",{class:"ls-marginV-1"},"该指令会在元素挂载时自动调用 `focus()` 方法，使元素获得焦点。",-1)),u((i(),o("div",null,[c(r(n),{code:x,"import-map":r(g),style:{height:"147px"}},null,8,["import-map"])])),[[l,t.value]])],64)}}});export{A as default};
---
+++ docs/assets/VirtualListTest-jAggnJ60.js
@@ -0,0 +1,15 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./VuePlayground-BZLSRyWc.js","./base-DbjQXbdW.js","./index-Cht9iEeR.js","./index-DH63uhMi.css","./base-CMRK1q-d.css","./el-scrollbar-keZdNopt.js","./install-B18KVwZb.js","./el-scrollbar-BWxh-h6K.css","./lsBaseLib.es-DNO7EYIp.js","./VuePlayground-BTNWep8G.css"])))=>i.map(i=>d[i]);
+import{d as n,j as p,a as i,A as r,z as u,f as d,u as e,a5 as c,F as f,a6 as _,o}from"./index-Cht9iEeR.js";import"./base-DbjQXbdW.js";import{v,i as g,a as V}from"./importMap-DlKZV69q.js";const x={style:{height:"285px"}},h=`<script setup>
+import {VirtualList} from 'ls-vue3-pro';
+import {ref} from 'vue';
+
+const list = ref(Array.from({length: 300}).map((x, i) => ({id: i, name:'Item - ' + i})));
+<\/script>
+
+<template>
+  <VirtualList :list-data="list" :item-height="30" item-key="id" class="height100" >
+    <template #default="{ item }">
+      {{ item.name }}
+    </template>
+  </VirtualList>
+</template>`,E=n({__name:"VirtualListTest",setup(y){const a=p(!0),m=c(async()=>{const s=await _(()=>import("./VuePlayground-BZLSRyWc.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return a.value=!1,s.default});return(s,t)=>{const l=v;return o(),i(f,null,[t[0]||(t[0]=r("div",{class:"ls-marginV-1"},"定高虚拟列表组件，用于高效渲染大量数据。",-1)),t[1]||(t[1]=r("div",{class:"ls-marginV-1"},"通过计算可视区域内的数据，减少 DOM 节点的渲染数量，提升性能。",-1)),u((o(),i("div",x,[d(e(m),{code:h,"import-map":e(V),css:e(g)},null,8,["import-map","css"])])),[[l,a.value]])],64)}}});export{E as default};
