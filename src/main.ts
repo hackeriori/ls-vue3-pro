3dea007c8f308ba52e37d532d7b7fe29d7739d67
Administrator <453843655@qq.com>
2025-04-19 14:21:11 +0800

    build(dependencies): 更新项目依赖并移除 Element Plus
    - 移除了 Element Plus 相关的导入和使用
    - 添加了 unplugin-auto-import 和 unplugin-vue-components 依赖

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import {createApp} from 'vue';
-import ElementPlus from 'element-plus'
-import zhCN from 'element-plus/es/locale/lang/zh-cn'
-import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
 import 'ls-base-lib/dist/css/ls.css';
 import './global.css'
 import App from './App.vue';
 import router from "./router";
 
-createApp(App).use(ElementPlus, {locale: zhCN}).use(router).mount('#app');
+createApp(App).use(router).mount('#app');
