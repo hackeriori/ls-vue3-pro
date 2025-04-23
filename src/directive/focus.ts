85f782079c5720af1582055765c0cbc623033939
Administrator <453843655@qq.com>
2025-04-23 18:41:32 +0800

    feat(directive): 升级 v-focus 指令功能 - 增加重新对焦次数功能，通过传递数值控制失焦后重新聚焦的次数 - 添加示例代码展示不同使用方式，包括直接聚焦、深层聚焦和重新聚焦次数 - 更新类型定义和文档，明确 v-focus指令的新功能 - 调整组件导入，移除未使用的 Element Plus 组件 - 版本号升级到 1.3.3

--- src/directive/focus.ts
+++ src/directive/focus.ts
@@ -2,12 +2,24 @@ import type {Directive} from 'vue';
 import type {DirectiveBinding} from '@vue/runtime-core';
 
 export const vFocus: Directive = {
-  mounted (_el: HTMLElement, binding: DirectiveBinding) {
+  mounted(_el: HTMLElement, binding: DirectiveBinding) {
     let el: HTMLElement | null = null;
+    // 重新对焦次数
+    let tryTimes = 0;
     if (binding.modifiers.deep)
       el = getDeepInput(_el);
     if (!el)
       el = _el;
+    // 用于肯定会失焦的场景，例如某些对话框内的输入框，会有失焦，此时需要重新聚焦
+    if (binding.value) {
+      const autoFocus = () => {
+        el.focus();
+        tryTimes++;
+        if (tryTimes >= binding.value)
+          el.removeEventListener('blur', autoFocus)
+      };
+      el.addEventListener('blur', autoFocus)
+    }
     el.focus()
   }
 }
--- src/directive/focusTest/FocusTest.vue
+++ src/directive/focusTest/FocusTest.vue
@@ -12,7 +12,9 @@ const VuePlayground = defineAsyncComponent(async () => {
 const showView = ref(1);
 
 function toggleShowView() {
-  showView.value = showView.value === 1 ? 2 : 1;
+  showView.value++;
+  if (showView.value > 3)
+    showView.value = 1;
 }
 
 const exampleCodeInput = `\<script setup\>
@@ -31,19 +33,35 @@ import {ElInput} from 'element-plus'
 \<template\>
   \<el-input v-focus.deep style="width: 177px" placeholder="自动聚焦的输入框"/\>
 \</template\>`;
+
+const exampleCodeRetry = `\<script setup\>
+import {vFocus} from 'ls-vue3-pro'
+import {ElInput} from 'element-plus'
+\</script\>
+
+\<template\>
+  \<el-input v-focus.deep="2" style="width: 177px" placeholder="自动聚焦的输入框"/\>
+\</template\>`;
 </script>
 
 <template>
   <div class="ls-marginV-1">v-focus指令，用于元素自动聚焦的自定义指令。</div>
   <div class="ls-marginV-1">该指令会在元素挂载时自动调用 `focus()` 方法，使元素获得焦点。</div>
+  <div class="ls-marginV-1">如果使用了 `.deep` 修饰符，则会递归查找子元素中的 `input` 或 `textarea` 并对其进行聚焦</div>
+  <div class="ls-marginV-1">通过传递数值给 `v-focus`，可以控制元素在失去焦点后重新聚焦的次数</div>
   <el-radio-group v-model="showView" class="ls-marginV-1">
     <el-radio :value="1" @click="toggleShowView">使用ElementPlus的输入框</el-radio>
     <el-radio :value="2" @click="toggleShowView">使用Input输入框</el-radio>
+    <el-radio :value="3" @click="toggleShowView">使用重新对焦次数</el-radio>
   </el-radio-group>
 
   <div v-loading="loadingExample">
-    <VuePlayground v-if="showView === 1" :code="exampleCodeElInput" :import-map="importMap" :css="importCss" style="height: 169px"></VuePlayground>
-    <VuePlayground v-else :code="exampleCodeInput" :import-map="importMap" :css="importCss" style="height: 147px"></VuePlayground>
+    <VuePlayground v-if="showView === 1" :code="exampleCodeElInput" :import-map="importMap" :css="importCss"
+                   style="height: 169px"></VuePlayground>
+    <VuePlayground v-else-if="showView === 2" :code="exampleCodeInput" :import-map="importMap" :css="importCss"
+                   style="height: 147px"></VuePlayground>
+    <VuePlayground v-else :code="exampleCodeRetry" :import-map="importMap" :css="importCss"
+                   style="height: 169px"></VuePlayground>
   </div>
 </template>
 
