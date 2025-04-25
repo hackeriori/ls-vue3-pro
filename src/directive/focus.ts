04c9acd70a170b69374029908e54c60debc122ff
Administrator <453843655@qq.com>
2025-04-25 13:42:21 +0800

    feat(focus): 增加初始不可见元素的聚焦功能 - 在 FocusTest.vue 中添加了新的示例代码，展示如何对初始不可见的元素进行聚焦 - 在 focus.ts 中实现了使用 IntersectionObserver 等待不可见元素变为可见后进行聚焦的功能 - 将版本号从 1.3.3 升级到 1.3.4

--- src/directive/focus.ts
+++ src/directive/focus.ts
@@ -4,14 +4,14 @@ import type {DirectiveBinding} from '@vue/runtime-core';
 export const vFocus: Directive = {
   mounted(_el: HTMLElement, binding: DirectiveBinding) {
     let el: HTMLElement | null = null;
-    // 重新对焦次数
-    let tryTimes = 0;
     if (binding.modifiers.deep)
       el = getDeepInput(_el);
     if (!el)
       el = _el;
     // 用于肯定会失焦的场景，例如某些对话框内的输入框，会有失焦，此时需要重新聚焦
     if (binding.value) {
+      // 重新对焦次数
+      let tryTimes = 0;
       const autoFocus = () => {
         el.focus();
         tryTimes++;
@@ -20,7 +20,20 @@ export const vFocus: Directive = {
       };
       el.addEventListener('blur', autoFocus)
     }
-    el.focus()
+    if (el.checkVisibility())
+      el.focus();
+    else{
+      // 如果元素不可见，则使用IntersectionObserver等待元素可见后对焦，适用于元素是v-show隐藏的场景
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            (entry.target as HTMLElement).focus();
+            observer.disconnect();
+          }
+        });
+      });
+      observer.observe(el);
+    }
   }
 }
 
--- src/directive/focusTest/FocusTest.vue
+++ src/directive/focusTest/FocusTest.vue
@@ -13,7 +13,7 @@ const showView = ref(1);
 
 function toggleShowView() {
   showView.value++;
-  if (showView.value > 3)
+  if (showView.value > 4)
     showView.value = 1;
 }
 
@@ -42,6 +42,21 @@ import {ElInput} from 'element-plus'
 \<template\>
   \<el-input v-focus.deep="2" style="width: 177px" placeholder="自动聚焦的输入框"/\>
 \</template\>`;
+
+const exampleCodeShow = `\<script setup\>
+import {ref} from 'vue'
+import {vFocus} from 'ls-vue3-pro';
+const isShow = ref(false);
+
+function toggleShow(){
+  isShow.value = !isShow.value
+}
+\</script\>
+
+\<template\>
+  <div><button @click="toggleShow">切换按钮</button></div>
+  \<input v-show="isShow" v-focus style="width: 177px" placeholder="自动聚焦的输入框"/\>
+\</template\>`;
 </script>
 
 <template>
@@ -50,9 +65,10 @@ import {ElInput} from 'element-plus'
   <div class="ls-marginV-1">如果使用了 `.deep` 修饰符，则会递归查找子元素中的 `input` 或 `textarea` 并对其进行聚焦</div>
   <div class="ls-marginV-1">通过传递数值给 `v-focus`，可以控制元素在失去焦点后重新聚焦的次数</div>
   <el-radio-group v-model="showView" class="ls-marginV-1">
-    <el-radio :value="1" @click="toggleShowView">使用ElementPlus的输入框</el-radio>
-    <el-radio :value="2" @click="toggleShowView">使用Input输入框</el-radio>
-    <el-radio :value="3" @click="toggleShowView">使用重新对焦次数</el-radio>
+    <el-radio :value="1" @click="toggleShowView">ElementPlus的输入框</el-radio>
+    <el-radio :value="2" @click="toggleShowView">Input输入框</el-radio>
+    <el-radio :value="3" @click="toggleShowView">重新对焦次数</el-radio>
+    <el-radio :value="4" @click="toggleShowView">初始不可见元素对焦</el-radio>
   </el-radio-group>
 
   <div v-loading="loadingExample">
@@ -60,7 +76,9 @@ import {ElInput} from 'element-plus'
                    style="height: 169px"></VuePlayground>
     <VuePlayground v-else-if="showView === 2" :code="exampleCodeInput" :import-map="importMap" :css="importCss"
                    style="height: 147px"></VuePlayground>
-    <VuePlayground v-else :code="exampleCodeRetry" :import-map="importMap" :css="importCss"
+    <VuePlayground v-else-if="showView === 3" :code="exampleCodeRetry" :import-map="importMap" :css="importCss"
+                   style="height: 147px"></VuePlayground>
+    <VuePlayground v-else :code="exampleCodeShow" :import-map="importMap" :css="importCss"
                    style="height: 169px"></VuePlayground>
   </div>
 </template>
